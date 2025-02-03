912c8d78219681b8073e09c3b5e8b979548fa401
Lee Morgan <lee@leemorgan.dev>
2025-02-03 16:59:02 -0500

    Add functionality to delete images from an album.

--- src/routes/dashboard/albums/EditAlbum.svelte
+++ src/routes/dashboard/albums/EditAlbum.svelte
@@ -44,9 +44,30 @@
             });
     }
 
-    const deletePhoto = (idx)=>{
-        console.log("index");
-        console.log(idx)
+    const deletePhoto = (id)=>{
+        loader.set(true);
+        fetch(`${import.meta.env.VITE_API_URL}/album/${album.id}/image/${id}`, {
+            method: "delete",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${userToken}`
+            }
+        })
+            .then(r=>r.json())
+            .then((response)=>{
+                if(response.error){
+                    notify("error", response.error.message);
+                }else{
+                    updateAlbum(response);
+                    album.photos = response.photos;
+                }
+            })
+            .catch((err)=>{
+                notify("error", "Something went wrong, try refreshing the page");
+            })
+            .finally(()=>{
+                loader.set(false);
+            });
     }
 </script>
 
@@ -86,9 +107,9 @@
             >
         </label>
 
-        {#each album.photos as photo, i}
+        {#each album.photos as photo}
             <div class="photo">
-                <button class="deletePhoto" aria-label="Delete Photo" onclick={()=>{deletePhoto(i)}}>
+                <button class="deletePhoto" aria-label="Delete Photo" onclick={()=>{deletePhoto(photo.id)}}>
                     <svg width="35" height="35" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2">
                         <polyline points="3 6 5 6 21 6"></polyline>
                         <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
