94dfee9ea254c9c4a8c58448731a8c39b61fcea8
Lee Morgan <lee@leemorgan.dev>
2025-02-05 11:24:36 -0500

    Fix issue with photo description still displaying entire album with scrolling.

--- src/routes/dashboard/albums/EditAlbum.svelte
+++ src/routes/dashboard/albums/EditAlbum.svelte
@@ -81,66 +81,66 @@
             updateAlbum={updateAlbum}
             close={()=>{photoDescription = null}}
         />
-    {/if}
-
-    <button class="closeIcon" onclick={()=>{changePage("main")}}>
-        <svg width="45px" height="45px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" color="#000000">
-            <path d="M6.75827 17.2426L12.0009 12M17.2435 6.75736L12.0009 12M12.0009 12L6.75827 6.75736M12.0009 12L17.2435 17.2426" stroke="#ff0000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-        </svg>
-    </button>
-
-    <EditName 
-        name={album.name}
-        albumId={album.id}
-        updateAlbum={updateAlbum}
-    />
-
-    <EditDescription
-        description={album.description}
-        albumId={album.id}
-        updateAlbum={updateAlbum}
-    />
-
-    <div class="divider"></div>
-
-    <div class="photos">
-        <label class="addPhotoButton">
-             <svg width="35px" height="35px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" color="#000000">
-                <path d="M8 12H12M16 12H12M12 12V8M12 12V16" stroke="#ff0000bf" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#ff0000bf" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+    {:else}
+        <button class="closeIcon" onclick={()=>{changePage("main")}}>
+            <svg width="45px" height="45px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" color="#000000">
+                <path d="M6.75827 17.2426L12.0009 12M17.2435 6.75736L12.0009 12M12.0009 12L6.75827 6.75736M12.0009 12L17.2435 17.2426" stroke="#ff0000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
             </svg>
-            <input
-                type="file"
-                bind:files={newImages}
-                onchange={addPhotos}
-                style="display:none"
-                multiple
-            >
-        </label>
+        </button>
 
-        {#each album.photos as photo}
-            <div class="photo">
-                <button class="deletePhoto" aria-label="Delete Photo" onclick={()=>{deletePhoto(photo.id)}}>
-                    <svg width="35" height="35" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2">
-                        <polyline points="3 6 5 6 21 6"></polyline>
-                        <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-                        <line x1="10" y1="11" x2="10" y2="17"></line>
-                        <line x1="14" y1="11" x2="14" y2="17"></line>
-                    </svg>
-                </button>
-                <img src={photoFile(photo)} alt={photo.file}>
-                <button
-                    class="photoDescription"
-                    onclick={()=>{photoDescription = photo}}
-                >{photo.description}</button>
-                <button
-                    aria-label="Edit description"
-                    class="mobileButton"
-                    onclick={()=>{photoDescription = photo}}
-                ></button>
-            </div>
-        {/each}
-    </div>
+        <EditName 
+            name={album.name}
+            albumId={album.id}
+            updateAlbum={updateAlbum}
+        />
+
+        <EditDescription
+            description={album.description}
+            albumId={album.id}
+            updateAlbum={updateAlbum}
+        />
+
+        <div class="divider"></div>
+
+        <div class="photos">
+            <label class="addPhotoButton">
+                 <svg width="35px" height="35px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" color="#000000">
+                    <path d="M8 12H12M16 12H12M12 12V8M12 12V16" stroke="#ff0000bf" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                    <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#ff0000bf" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                </svg>
+                <input
+                    type="file"
+                    bind:files={newImages}
+                    onchange={addPhotos}
+                    style="display:none"
+                    multiple
+                >
+            </label>
+
+            {#each album.photos as photo}
+                <div class="photo">
+                    <button class="deletePhoto" aria-label="Delete Photo" onclick={()=>{deletePhoto(photo.id)}}>
+                        <svg width="35" height="35" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2">
+                            <polyline points="3 6 5 6 21 6"></polyline>
+                            <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                            <line x1="10" y1="11" x2="10" y2="17"></line>
+                            <line x1="14" y1="11" x2="14" y2="17"></line>
+                        </svg>
+                    </button>
+                    <img src={photoFile(photo)} alt={photo.file}>
+                    <button
+                        class="photoDescription"
+                        onclick={()=>{photoDescription = photo}}
+                    >{photo.description}</button>
+                    <button
+                        aria-label="Edit description"
+                        class="mobileButton"
+                        onclick={()=>{photoDescription = photo}}
+                    ></button>
+                </div>
+            {/each}
+        </div>
+    {/if}
 </div>
 
 <style>
